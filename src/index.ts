fef91b43df89f7f608db83eb1e676f0180312418
Андрей Епифанов <epifanovmd@gmail.com>
2025-07-06 12:22:10 +0300

    fix poll

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,9 @@ const start = async () => {
     subscribePoll();
 
     bot.command("start", async ctx => {
-      await createPoll(ctx);
-
-      // if (ctx.message) {
-      //   await ctx.api.deleteMessage(ctx.chatId, ctx.message?.message_id);
-      // }
+      if (ctx.message) {
+        await ctx.api.deleteMessage(ctx.chatId, ctx.message?.message_id);
+      }
 
       if (ctx.chat.type === "supergroup") {
         if (!running.has(ctx.chatId)) {
