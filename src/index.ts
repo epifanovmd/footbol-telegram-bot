0ad90dfe1f238c3cc54f5f522ddf7ce87672e143
Андрей Епифанов <epifanovmd@gmail.com>
2025-07-06 12:26:47 +0300

    fix poll

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Context } from "grammy";
 import moment from "moment";
 import schedule from "node-schedule";
 
@@ -15,6 +16,27 @@ let messagesCount = 0;
 const NOT_ACCEPT_MESSAGE =
   "Братуха, я работаю только с командами!\n\nДобавь меня в свою группу, и я буду создавать и управлять опросами для сбора игроков каждый день в 12:00";
 
+const runBot = async (ctx: Context) => {
+  if (ctx.chat && ctx.chat.type === "supergroup") {
+    if (ctx.chatId && !running.has(ctx.chatId)) {
+      running.add(ctx.chatId);
+
+      // Правило: каждый день в 12:00 по Москве
+      const rule = new schedule.RecurrenceRule();
+      rule.hour = 12; // 12 часов
+      rule.minute = 0; // 0 минут
+      rule.tz = "Europe/Moscow"; // Указываем часовой пояс
+
+      // Ежедневно в 12:00 по Москве
+      schedule.scheduleJob(rule, async () => {
+        await createPoll(ctx);
+      });
+    }
+  } else {
+    await ctx.reply(NOT_ACCEPT_MESSAGE);
+  }
+};
+
 const start = async () => {
   try {
     subscribePoll();
@@ -24,27 +46,12 @@ const start = async () => {
         await ctx.api.deleteMessage(ctx.chatId, ctx.message?.message_id);
       }
 
-      if (ctx.chat.type === "supergroup") {
-        if (!running.has(ctx.chatId)) {
-          running.add(ctx.chatId);
-
-          // Правило: каждый день в 12:00 по Москве
-          const rule = new schedule.RecurrenceRule();
-          rule.hour = 12; // 12 часов
-          rule.minute = 0; // 0 минут
-          rule.tz = "Europe/Moscow"; // Указываем часовой пояс
-
-          // Ежедневно в 12:00 по Москве
-          schedule.scheduleJob(rule, async () => {
-            await createPoll(ctx);
-          });
-        }
-      } else {
-        await ctx.reply(NOT_ACCEPT_MESSAGE);
-      }
+      await runBot(ctx);
     });
 
     bot.on("message", async ctx => {
+      await runBot(ctx);
+
       if (ctx.chat.type === "supergroup") {
         await receiveMessageVote(ctx);
 
